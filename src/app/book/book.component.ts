32347c4468a0681f80b14b858edd17d0cb794aac
Майринова Марина <mayrinova@evolenta.ru>
2024-07-22 16:54:18 +0300

    Практическое задание 6: Сервисы

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {BookService} from "../book.service";
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+  constructor(
+      public bookServ: BookService
+  ) { }
+  ngOnInit(): void {
+  }
+}
