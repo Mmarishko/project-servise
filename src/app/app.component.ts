4f40732bb2c0ff5266033b04b04695bf6da1d05f
Майринова Марина <mayrinova@evolenta.ru>
2024-07-23 12:37:06 +0300

    Практическое задание8: Интеграция UI библиотек

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,102 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project_servise';
+
+  accordionGroups: any[] = [
+    {
+      header: 'Static Header',
+      content: 'This content is straight in the template.'
+    },
+    {
+      header: 'Another group',
+      content: 'Another content'
+    },
+    {
+      header: 'Some another group',
+      content: 'Some another content'
+    },
+    {
+      header: 'Last group',
+      content: 'Last content'
+    }
+  ]
+
+  data = [
+    {
+      "name": "Germany",
+      "value": 40632,
+      "extra": {
+        "code": "de"
+      }
+    },
+    {
+      "name": "United States",
+      "value": 50000,
+      "extra": {
+        "code": "us"
+      }
+    },
+    {
+      "name": "France",
+      "value": 36745,
+      "extra": {
+        "code": "fr"
+      }
+    },
+    {
+      "name": "United Kingdom",
+      "value": 36240,
+      "extra": {
+        "code": "uk"
+      }
+    },
+    {
+      "name": "Spain",
+      "value": 33000,
+      "extra": {
+        "code": "es"
+      }
+    },
+    {
+      "name": "Italy",
+      "value": 35800,
+      "extra": {
+        "code": "it"
+      }
+    }
+  ]
+
+  notiflix_notify(type: string) {
+    switch (type) {
+      case 'success':
+        Notify.success('This is success alert!');
+        break;
+      case 'warning':
+        Notify.warning('This is warning alert!');
+        break;
+      case 'info':
+        Notify.info('This is info alert!');
+        break;
+      case 'failure':
+        Notify.failure('This is error alert!');
+        break;
+    }
+  }
+
+  notiflix_confirm(text: string) {
+    Confirm.show(
+        'Notiflix Confirm',
+        text,
+        'Yes',
+        'No',
+        () => {
+          alert('Thank you.');
+        },
+        () => {
+          alert('If you say so...');
+        },
+        {
+        },
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
