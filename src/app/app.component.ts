18d517ffbe6c0d04ca76111806bbe4de16f4bfd5
Майринова Марина <mayrinova@evolenta.ru>
2024-07-26 21:23:34 +0300

    Практическое задание9: TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ResponseService} from "./response.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'project_servise';
+  myUsers = this.response.arrUsers;
+  constructor(
+      private response: ResponseService,
+  ){}
+  ngOnInit() {
+  }
 }
