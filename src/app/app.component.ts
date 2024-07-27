75cf3a27c229c90c0cc65b382b8806988086e38f
Майринова Марина <mayrinova@evolenta.ru>
2024-07-27 09:39:44 +0300

    Практическое задание10: Валидация форм

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Books {
+  title: string | null;
+  author: string | null;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project_servise';
+export class AppComponent implements OnInit {
+  title = 'forms 2.2';
+
+  books_list: Books[] = [
+    {
+      title: 'Евгений Онегин',
+      author: 'Александр Пушкин',
+    },
+    {
+      title: 'Мертвые души',
+      author: 'Николая Гоголь',
+    },
+    {
+      title: 'Война и мир',
+      author: 'Лев Толстой',
+    },
+  ];
+  create_book = {
+    title: null,
+    author: null,
+  };
+
+  ngOnInit() {
+
+  }
+
+  createBook() {
+    this.books_list.push({
+      title: this.create_book.title,
+      author: this.create_book.author,
+    });
+  }
 }
